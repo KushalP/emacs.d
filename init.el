6fad08f26a06aa27b6fcd06793888cec873cf6cf
Kushal Pisavadia <kushi.p@gmail.com>
2015-01-04 13:00:20 +0000

    Initialise emacs config
    This is a barebones minimal config which uses babel (org-mode formatted)
    config to define how we want emacs to be configured.
    
    For now it has some sane defaults that we can build upon.

---
+++ init.el
@@ -0,0 +1,25 @@
+(require 'package)
+
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("org" . "http://orgmode.org/elpa/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+
+(package-initialize)
+
+;; Run this early on. Suppress the startup message flash on the
+;; screen.
+(setq inhibit-startup-message t)
+
+;; This bootstraps us if we don't have anything.
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; org-mode always needs to be installed if it's not already present
+(when (not (package-installed-p 'org-plus-contrib))
+  (package-install 'org-plus-contrib))
+
+(require 'org)
+
+;; Load the main config file.
+(org-babel-load-file (concat user-emacs-directory "org/config.org"))
